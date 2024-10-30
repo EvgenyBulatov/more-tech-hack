ddbd3565e2d320e07bf374778445411af5b13981
ebulatov <yabulatov@mts.ru>
2024-10-30 19:34:42 +0300

    final hack commit

--- delta_lake_solution/join_delta.py
+++ delta_lake_solution/join_delta.py
@@ -5,20 +5,20 @@ from pyspark.conf import SparkConf
 import time
 from delta import *
 
-level = "3"  # Какую таблицу тестируем, маленькую, среднюю или большую
-your_bucket_name = "result"  # Имя вашего бакета
-your_access_key = "***"  # Ключ от вашего бакета
-your_secret_key = "***"  # Ключ от вашего бакета
+level = "*" #Какую таблицу тестируем, маленькую, среднюю или большую
+your_bucket_name = "result" #Имя вашего бакета
+your_access_key = "***" #Ключ от вашего бакета
+your_secret_key = "***" #Ключ от вашего бакета
 
 configs = {
-    "spark.sql.files.maxPartitionBytes": "1073741824", #1GB
+    "spark.sql.files.maxPartitionBytes": "1073741824",  # 1GB
     "spark.hadoop.fs.s3a.impl": "org.apache.hadoop.fs.s3a.S3AFileSystem",
     "spark.hadoop.fs.s3a.path.style.access": "true",
     "spark.hadoop.fs.s3a.connection.ssl.enabled": "true",
     "spark.hadoop.fs.s3a.fast.upload": "true",
-    "spark.hadoop.fs.s3a.block.size": "134217728", # 128MB
-    "spark.hadoop.fs.s3a.multipart.size": "268435456", # 256MB
-    "spark.hadoop.fs.s3a.multipart.threshold": "536870912", # 512MB
+    "spark.hadoop.fs.s3a.block.size": "134217728",  # 128MB
+    "spark.hadoop.fs.s3a.multipart.size": "268435456",  # 256MB
+    "spark.hadoop.fs.s3a.multipart.threshold": "536870912",  # 512MB
     "spark.hadoop.fs.s3a.committer.name": "magic",
     "spark.hadoop.fs.s3a.bucket.all.committer.magic.enabled": "true",
     "spark.hadoop.fs.s3a.threads.max": "64",
@@ -30,21 +30,23 @@ configs = {
     "spark.hadoop.fs.s3a.bucket.source-data.secret.key": "IDkOoR8KKmCuXc9eLAnBFYDLLuJ3NcCAkGFghCJI",
     f"spark.hadoop.fs.s3a.bucket.{your_bucket_name}.access.key": your_access_key,
     f"spark.hadoop.fs.s3a.bucket.{your_bucket_name}.secret.key": your_secret_key,
-    "spark.sql.parquet.compression.codec": "snappy",
+    "spark.sql.orc.compression.codec": "snappy",
     "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension", # подтягиваем delta-зависимости
     "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog", # подтягиваем delta-зависимости
-    "spark.databricks.delta.retentionDurationCheck.enabled": "false", # параметр для корректной работы с .vacuum()
-    "spark.dynamicAllocation.enabled": "true", # используем динамическую аллокацию для грамотного расчета ресурсов спарка
-    "spark.dynamicAllocation.minExecutors": "0", # минимальное кол-во экзекьютеров при дин. алл.
-    "spark.dynamicAllocation.maxExecutors": "7", # максимальное кол-во экзекьютеров при дин. алл.
-    "spark.dynamicAllocation.schedulerBacklogTimeout": "4s", # максимальное время ожидания доп исполнителей
-    "spark.dynamicAllocation.executorIdleTimeout": "400s" # макс время бездецствия экзекьютера
+    "spark.driver.memory": "6g",
+    "spark.driver.cores": "1",
+    "spark.executor.memory": "14g",
+    "spark.executor.cores": "4",
+    "spark.executor.instances": "8",
+    "spark.default.parallelism": "56",
+    "spark.sql.shuffle.partitions": "56"
 }
 conf = SparkConf()
 conf.setAll(configs.items())
 
 # создаем сессию
-spark = SparkSession.builder.config(conf=conf).getOrCreate()
+builder = SparkSession.builder.config(conf=conf)
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
 sc = spark.sparkContext
 log = spark._jvm.org.apache.log4j.LogManager.getLogger(">>> App")
 
@@ -58,41 +60,52 @@ start = time.perf_counter()
 
 # таблицы
 increment_table = spark.read.parquet(incr_table)
-target_delta_table = DeltaTable.forPath(spark, init_table) # используем именно дельта таблицу
+target_table = spark.read.format("delta").load(init_table)
+
+target_columns = target_table.columns
 
 # добавляем колонки партиционирования в приходящий инкремент
-target_columns = target_delta_table.toDF().columns
 increment = (increment_table
-    .withColumn("eff_from_month", last_day(col("eff_from_dt")).cast(StringType()))
-    .withColumn("eff_to_month", last_day(col("eff_to_dt")).cast(StringType()))
-    .repartition("eff_to_month", "eff_from_month")
-    .selectExpr(target_columns)
-)
+             .withColumn("eff_from_month", last_day(col("eff_from_dt")))
+             .withColumn("eff_to_month", last_day(col("eff_to_dt")))
+             .repartition("eff_to_month", "eff_from_month")
+             .selectExpr(target_columns)
+             )
+# по сути, новые данные с закрытой датой
+increment_closed = increment \
+    .filter(col("eff_to_month") != lit("5999-12-31")).select("id", "eff_from_month", "eff_from_dt").cache()
 
-# реализация логики SCD2 через delta
-(target_delta_table.alias('tgt') \
-    .merge(
-        increment.alias('incr'), # сначала условие выбора строк, над которым будем проводить обновление (правило)
-        "tgt.eff_to_month = '5999-12-31' \
-        AND incr.eff_to_month != '5999-12-31' \
-        AND incr.eff_from_month = tgt.eff_from_month \
-        AND tgt.eff_from_dt = incr.eff_from_dt \
-        AND tgt.id = incr.id"
-    )
-    .whenMatchedUpdate(
-        set= { # затем на что именно заменяем строки с обновлениями
-            "eff_to_dt": "incr.eff_to_dt",
-            "eff_to_month": "incr.eff_to_month",
-        }
-    )
-    .whenNotMatchedInsertAll()
-    .execute()
-)
+# берем список партиций из инкремента, так как именно по ним могут прийти апдейты
+rows = increment_closed.select(col("eff_from_month") \
+                                          .cast(StringType())).distinct().orderBy("eff_from_month").collect()
 
-target_delta_table.vacuum(0) # отчистка ненужной версии данных, таким образом оставляем только нужные нам по логике работы SCD2
+partitions = [str(row[0]) for row in rows]
 
-# сохраняем данные тоже в формате delta
-target_delta_table.toDF().write.format("delta").mode("overwrite").option("overwriteSchema", "true").save(init_table)
+# проходимся по партициям инкремента
+for from_dt in partitions:
+    log.info(f"<-start working with partition {from_dt}->")
+
+    # берем только нужную партицию инита
+    target_partition = target_table \
+        .filter(col("eff_to_month") == lit("5999-12-31")) \
+        .filter(col("eff_from_month") == from_dt)
+
+    # закрытые записи инкремента
+    closed_increment_partitioning = increment_closed \
+        .filter(col("eff_from_month") == from_dt)
+
+    # открытые записи без изменений
+    old_data = target_partition.join(closed_increment_partitioning, on=["id", "eff_from_dt"], how='left_anti')
+
+    old_data.write. \
+        format("delta"). \
+        mode("overwrite"). \
+        partitionBy("eff_to_month", "eff_from_month"). \
+        option("partitionOverwriteMode", "dynamic"). \
+        save(init_table)
+
+# аппендим оставшиеся данные инкремента в init
+increment.write.format("delta").mode("append").save(init_table)
 
 end = time.perf_counter() - start
 
